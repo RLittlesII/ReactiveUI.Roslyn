4f01473688588b98d3d904a4af68b4d9a0697199
Rodney Littles II <6969701+RLittlesII@users.noreply.github.com>
2022-02-02 16:11:24 -0600

    chore: fixing NuGet publishing (#12)
    * chore: fixing NuGet publishing
    
    * adding Rocket.Surger.MsBuild.GitVersion

--- .build/ContinuousDelivery.cs
+++ .build/ContinuousDelivery.cs
@@ -11,12 +11,12 @@ using Rocket.Surgery.Nuke;
 using Rocket.Surgery.Nuke.ContinuousIntegration;
 using Rocket.Surgery.Nuke.DotNetCore;
 using Rocket.Surgery.Nuke.GithubActions;
+using System.Collections.Generic;
+using System.Linq;
 
 [PublicAPI]
 [CheckBuildProjectConfigurations]
 [UnsetVisualStudioEnvironmentVariables]
-// [PackageIcon("https://raw.githubusercontent.com/RocketSurgeonsGuild/graphics/master/png/social-square-thrust-rounded.png")]
-[EnsureGitHooks(GitHook.PreCommit)]
 [EnsureReadmeIsUpdated("Readme.md")]
 [DotNetVerbosityMapping]
 [MSBuildVerbosityMapping]
@@ -57,12 +57,12 @@ class ContinuousDelivery : NukeBuild,
 
     [OptionalGitRepository] public GitRepository? GitRepository { get; }
 
+    [ComputedGitVersion] public GitVersion GitVersion { get; } = null!;
+
     public Target Build => _ => _.Inherit<ICanBuildWithDotNetCore>(x => x.CoreBuild);
 
     public Target Pack => _ => _.Inherit<ICanPackWithDotNetCore>(x => x.CorePack)
        .DependsOn(Clean);
-
-    [ComputedGitVersion] public GitVersion GitVersion { get; } = null!;
 
     public Target Clean => _ => _.Inherit<ICanClean>(x => x.Clean);
     public Target Restore => _ => _.Inherit<ICanRestoreWithDotNetCore>(x => x.CoreRestore);
@@ -80,4 +80,59 @@ class ContinuousDelivery : NukeBuild,
        .DependsOn(Pack);
 
     [Parameter("Configuration to build")] public Configuration Configuration { get; } = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    public static RocketSurgeonGitHubActionsConfiguration Middleware(RocketSurgeonGitHubActionsConfiguration configuration)
+    {
+        var buildJob = configuration.Jobs.First(z => z.Name == "Build");
+        var checkoutStep = buildJob.Steps.OfType<CheckoutStep>().Single();
+        // For fetch all
+        checkoutStep.FetchDepth = 0;
+        buildJob.Steps.InsertRange(buildJob.Steps.IndexOf(checkoutStep) + 1, new BaseGitHubActionsStep[] {
+            new RunStep("Fetch all history for all tags and branches") {
+                Run = "git fetch --prune"
+            },
+            new SetupDotNetStep("Use .NET Core 3.1 SDK") {
+                DotNetVersion = "3.1.x"
+            }
+        });
+
+        buildJob.Steps.Add(new UsingStep("Publish Coverage")
+        {
+            Uses = "codecov/codecov-action@v1",
+            With = new Dictionary<string, string>
+            {
+                ["name"] = "actions-${{ matrix.os }}",
+            }
+        });
+
+        buildJob.Steps.Add(new UploadArtifactStep("Publish logs")
+        {
+            Name = "logs",
+            Path = "artifacts/logs/",
+            If = "always()"
+        });
+
+        buildJob.Steps.Add(new UploadArtifactStep("Publish coverage data")
+        {
+            Name = "coverage",
+            Path = "coverage/",
+            If = "always()"
+        });
+
+        buildJob.Steps.Add(new UploadArtifactStep("Publish test data")
+        {
+            Name = "test data",
+            Path = "artifacts/test/",
+            If = "always()"
+        });
+
+        buildJob.Steps.Add(new UploadArtifactStep("Publish NuGet Packages")
+        {
+            Name = "nuget",
+            Path = "artifacts/nuget/",
+            If = "always()"
+        });
+
+        return configuration;
+    }
 }
