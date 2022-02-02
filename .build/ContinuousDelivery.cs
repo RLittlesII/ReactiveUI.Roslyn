405d56f78b60d4fb3ee85c4fc8082fdb82461251
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2022-02-02 10:24:05 -0600

    chore: fixing NuGet publishing

--- .build/ContinuousDelivery.cs
+++ .build/ContinuousDelivery.cs
@@ -11,6 +11,8 @@ using Rocket.Surgery.Nuke;
 using Rocket.Surgery.Nuke.ContinuousIntegration;
 using Rocket.Surgery.Nuke.DotNetCore;
 using Rocket.Surgery.Nuke.GithubActions;
+using System.Collections.Generic;
+using System.Linq;
 
 [PublicAPI]
 [CheckBuildProjectConfigurations]
@@ -57,6 +59,8 @@ class ContinuousDelivery : NukeBuild,
 
     [OptionalGitRepository] public GitRepository? GitRepository { get; }
 
+    [ComputedGitVersion] public GitVersion GitVersion { get; } = null!;
+
     public Target Build => _ => _.Inherit<ICanBuildWithDotNetCore>(x => x.CoreBuild);
 
     public Target Pack => _ => _.Inherit<ICanPackWithDotNetCore>(x => x.CorePack)
@@ -80,4 +84,59 @@ class ContinuousDelivery : NukeBuild,
        .DependsOn(Pack);
 
     [Parameter("Configuration to build")] public Configuration Configuration { get; } = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    public static RocketSurgeonGitHubActionsConfiguration Middleware(RocketSurgeonGitHubActionsConfiguration configuration)
+    {
+        var buildJob = configuration.Jobs.First(z => z.Name == "Build");
+        var checkoutStep = buildJob.Steps.OfType<CheckoutStep>().Single();
+        // For fetch all
+        checkoutStep.FetchDepth = 0;
+        buildJob.Steps.InsertRange(buildJob.Steps.IndexOf(checkoutStep) + 1, new BaseGitHubActionsStep[] {
+            new RunStep("Fetch all history for all tags and branches") {
+                Run = "git fetch --prune"
+            },
+            new SetupDotNetStep("Use .NET Core 3.1 SDK") {
+                DotNetVersion = "3.1.x"
+            }
+        });
+
+        buildJob.Steps.Add(new UsingStep("Publish Coverage")
+        {
+            Uses = "codecov/codecov-action@v1",
+            With = new Dictionary<string, string>
+            {
+                ["name"] = "actions-${{ matrix.os }}",
+            }
+        });
+
+        buildJob.Steps.Add(new UploadArtifactStep("Publish logs")
+        {
+            Name = "logs",
+            Path = "artifacts/logs/",
+            If = "always()"
+        });
+
+        buildJob.Steps.Add(new UploadArtifactStep("Publish coverage data")
+        {
+            Name = "coverage",
+            Path = "coverage/",
+            If = "always()"
+        });
+
+        buildJob.Steps.Add(new UploadArtifactStep("Publish test data")
+        {
+            Name = "test data",
+            Path = "artifacts/test/",
+            If = "always()"
+        });
+
+        buildJob.Steps.Add(new UploadArtifactStep("Publish NuGet Packages")
+        {
+            Name = "nuget",
+            Path = "artifacts/nuget/",
+            If = "always()"
+        });
+
+        return configuration;
+    }
 }
