9fe84b9dbe5e6dea9be7abb297323f94df0737ff
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2022-08-14 20:00:10 -0500

    add CodeFixProviderBase

---
+++ src/RxUI.Analysis.Roslyn/CodeFixProviderBase.cs
@@ -0,0 +1,49 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReactiveUI.Analysis.Roslyn
+{
+    public abstract class CodeFixProviderBase : CodeFixProvider
+    {
+        private const string Title = "Fix out parameter assignment";
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            // Find the type declaration identified by the diagnostic.
+            var ancestorsAndSelf = (root?.FindToken(diagnosticSpan.Start).Parent?.AncestorsAndSelf() ?? Array.Empty<SyntaxNode>()).ToList();
+            var invocation = ancestorsAndSelf.OfType<InvocationExpressionSyntax>().First();
+            var declaration = ancestorsAndSelf.OfType<SimpleLambdaExpressionSyntax>().First();
+            var variableDeclaration = ancestorsAndSelf.OfType<LocalDeclarationStatementSyntax>().First();
+
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: Title,
+                    createChangedDocument: c => Fixup(context.Document,variableDeclaration, invocation, declaration, c),
+                    equivalenceKey: OutParameterAssignmentAnalyzer.Rule.Id + OutParameterAssignmentAnalyzer.Rule.Title),
+                diagnostic);
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+        private Task<Document> Fixup(
+            Document document,
+            LocalDeclarationStatementSyntax declarationSyntax,
+            InvocationExpressionSyntax invocation,
+            SimpleLambdaExpressionSyntax declaration,
+            CancellationToken cancellationToken
+        ) => Fix(document,declarationSyntax, invocation, declaration, cancellationToken);
+
+        protected virtual Task<Document> Fix(Document document, LocalDeclarationStatementSyntax declarationSyntax, InvocationExpressionSyntax invocation, SimpleLambdaExpressionSyntax declaration, CancellationToken cancellationToken) => Task.FromResult(document);
+    }
+}
--- src/RxUI.Analysis.Roslyn/rxui0007/SubscriptionDisposalCodeFixProvider.cs
+++ src/RxUI.Analysis.Roslyn/rxui0007/SubscriptionDisposalCodeFixProvider.cs
@@ -1,15 +1,32 @@
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Collections.Immutable;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ReactiveUI.Analysis.Roslyn
 {
-    public class SubscriptionDisposalCodeFixProvider : CodeFixProvider
+    public class SubscriptionDisposalCodeFixProvider : CodeFixProviderBase
     {
         private const string Title = "Add DisposeWith to subscription.";
 
-        public override Task RegisterCodeFixesAsync(CodeFixContext context) => Task.CompletedTask;
-
         public sealed override ImmutableArray<string> FixableDiagnosticIds { get; } = ImmutableArray.Create(SubscriptionDisposalAnalyzer.Rule.Id);
+
+        protected override async Task<Document> Fix(
+            Document document,
+            LocalDeclarationStatementSyntax declarationSyntax,
+            InvocationExpressionSyntax invocation,
+            SimpleLambdaExpressionSyntax declaration,
+            CancellationToken cancellationToken
+        )
+        {
+            var rootAsync = await document.GetSyntaxRootAsync(cancellationToken);
+            if (true)
+            {
+
+            }
+            return await base.Fix(document, declarationSyntax, invocation, declaration, cancellationToken);
+        }
     }
 }
