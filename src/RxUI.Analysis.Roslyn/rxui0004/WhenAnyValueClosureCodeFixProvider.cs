99e2eb320ecbcbc328903583b2a73712c8c39131
Rodney Littles II <6969701+RLittlesII@users.noreply.github.com>
2023-03-25 12:45:58 -0500

    chore: rename closer to closure because we have class (#21)

--- src/RxUI.Analysis.Roslyn/rxui0004/WhenAnyValueClosureCodeFixProvider.cs
+++ src/RxUI.Analysis.Roslyn/rxui0004/WhenAnyValueClosureCodeFixProvider.cs
@@ -5,7 +5,6 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Collections.Immutable;
 using System.Composition;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
